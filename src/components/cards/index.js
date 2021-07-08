f8f043354cf0ec5be985487ede71f1818cfb4d44
unname666 <78314985+unname666@users.noreply.github.com>
2021-07-08 10:30:52 -0700

    working on the layout

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
 
-function cards(props) {
-    return(
-        <div className="card">
-  
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text"></p>
-  </div>
-  </div>
-    )
-    }
+function cards({ details, image, title }) {
+  return (
+    <>
+      <div className="jumbotron card-container">
+        <Row>
+          <Col xs={6} md={4}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{details}</Card.Text>
+                {/* <Button variant="primary">Go somewhere</Button> */}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
 
-    export default cards
+export default cards;
+
+// <div key={mission_name}>
+//           <h1>🛰 {mission_name}</h1>
+//           <p>{details}</p>
+//           <img src={(links.flickr_images)} width="200" />
+//         </div>
